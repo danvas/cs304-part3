9ea5bb0cb11a27078a11b4c1d7a963d2f9834c0b
bigchip <ianchipperfield@gmail.com>
2013-07-31 11:28:24 -0700

    Here's the tutorial GUI

---
+++ src/GUI.java
@@ -0,0 +1,32 @@
+//package start;
+import javax.swing.*;
+
+
+
+public class GUI {
+
+private static void createandshowGUI(){
+	
+	//creates and prepares window
+	JFrame frame = new JFrame("Gui");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	//add AMS Database Label
+	JLabel label = new JLabel("AMS Database");
+	frame.getContentPane().add(label);
+	
+	//Display the window
+	frame.pack();
+	frame.setVisible(true);
+}
+
+public static void main(String[] args){
+	//schedule job for event-dispatching thread
+	//creating  and showing this application's GUI
+	javax.swing.SwingUtilities.invokeLater(new Runnable(){
+		public void run(){
+			createandshowGUI();
+		}
+	});
+	}
+}
