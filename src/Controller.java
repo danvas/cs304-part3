26b4eda55a6b9eb3eb338518aad9b7c80364cc21
bigchip <ianchipperfield@gmail.com>
2013-08-07 10:27:21 -0700

    Added a new Controller class. This will be used to manage exceptions thrown so we can put them in an error dialog. It's also used to provide instances of all TableOperations classes to MainFrame

---
+++ src/Controller.java
@@ -0,0 +1,45 @@
+
+public class Controller implements ExceptionListener{
+
+	private HasSongOperations hso = null;
+	private CustomerOperations co = null;
+	private ItemOperations io = null;
+	private LeadSingerOperations lso = null;
+	private PurchaseOperations p = null;
+	private PurchaseItemOperations pi = null;
+	private ReturnOperations ro = null;
+	private ReturnItemOperations rio = null;
+	
+	@Override
+	public void exceptionGenerated(ExceptionEvent ex) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Controller(){
+		this.hso = new HasSongOperations();
+		this.co = new CustomerOperations();
+		this.io = new ItemOperations();
+		this.lso = new LeadSingerOperations();
+		this.p = new PurchaseOperations();
+		this.pi = new PurchaseItemOperations();
+		this.ro = new ReturnOperations();
+		this.rio = new ReturnItemOperations();
+		
+		//Add this as exception listener to all operations classes
+		hso.addExceptionListener(this);
+		co.addExceptionListener(this);
+		io.addExceptionListener(this);
+		lso.addExceptionListener(this);
+		p.addExceptionListener(this);
+		pi.addExceptionListener(this);
+		ro.addExceptionListener(this);
+		rio.addExceptionListener(this);
+	}
+	public PurchaseOperations getPurchaseOps(){
+		return this.p;
+	}
+	public CustomerOperations getCustomerOps(){
+		return this.co;
+	}
+	
+}
